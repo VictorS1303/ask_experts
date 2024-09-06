add69653ccaec70a96990dded24970f526c4c19f
Victor Schweitz <victorschweitz@hotmail.com>
2024-09-06 12:29:57 +0200

    Icons set up

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-    devToolbar:
-    {
-        enabled: false,
-    }
+  devToolbar:
+  {
+      enabled: false,
+  },
+
+  integrations: [icon()]
 });
